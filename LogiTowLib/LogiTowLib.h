0ae76257c397cef68a3326727f929f5352382bfa
Trychen <trychen@live.com>
2017-12-10 01:24:06 +0800

    Initial Commit

---
+++ LogiTowLib/LogiTowLib.h
@@ -0,0 +1,13 @@
+//
+//  LogiTowLib.h
+//  LogiTowLib
+//
+//  Created by Trychen on 17/12/10.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LogiTowLib : NSObject
+
+@end
---
+++ LogiTowLib/LogiTowLib.m
@@ -0,0 +1,13 @@
+//
+//  LogiTowLib.m
+//  LogiTowLib
+//
+//  Created by Trychen on 17/12/10.
+//
+//
+
+#import "LogiTowLib.h"
+
+@implementation LogiTowLib
+
+@end
